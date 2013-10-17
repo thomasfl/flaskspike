e17fa823b94654c8917103a274305ec56b49dbd0
thomasfl <thomas.flemming@gmail.com>
2013-10-17 10:29:59 +0200

    initial commit

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def test_entry():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['GET','POST'])
 def hello():
-    return 'Hello World!'
+    result = -1
+    try:
+        result = int(request.form['a']) + int(request.form['b'])
+    except Exception:
+        pass
+    return render_template('index.html', result=result)
+
+if(not(os.environ.has_key("PRODUCTION_MODE"))):
+    app.run(debug=True)
